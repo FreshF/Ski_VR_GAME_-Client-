4f7c33497807db51496ed384d32b52ed8736921d
FreshF <49163804+FreshF@users.noreply.github.com>
2020-05-08 12:15:30 +0200

    Import Dummy Game (Client)

---
+++ dummy (Client)/Assets/Code/Networking/NetworkClient.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+namespace Project.Networking 
+{
+    public class NetworkClient : SocketIOComponent 
+    {
+    
+        // Start is called before the first frame update
+        public override void Start() 
+        {
+            base.Start();        
+        }
+
+        // Update is called once per frame
+        public virtual void Update() 
+        {
+            base.Update();        
+        }
+    }
+}
